31b9b9b761e4f934b0df4b2c54e391e0fbef61f8
nelisabe <aleksandr.engoyan@gmail.com>
2021-05-04 16:51:35 +0300

    add dns search program. just for fun..

---
+++ main.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/04 11:36:11 by nelisabe          #+#    #+#             */
+/*   Updated: 2021/05/04 16:19:21 by nelisabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <iostream>
+# include <string>
+# include <netinet/in.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+# include <netdb.h>
+# include <arpa/inet.h>
+
+typedef	struct sockaddr_in	t_sockaddr;
+typedef struct addrinfo 	t_addrinfo;
+
+#endif
