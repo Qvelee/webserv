cdbcf86193cf8250443e321c6bf90cb73d21c661
gmorros <gmorros@student.21-school.ru>
2021-07-16 17:31:19 +0300

    fix

--- http/test_http/test_full_message.cpp
+++ http/test_http/test_full_message.cpp
@@ -42,3 +42,22 @@ TEST(TestConnection, one) {
   http::parse_request(current, message);
   ASSERT_EQ(current, expected);
 }
+
+TEST(Test, one) {
+
+  std::string message = "GET / HTTP/1.1\r\n"
+						"Host: localhost:8080\r\n"
+						"\r\n";
+  http::Request expected = {
+	  .method = http::GET,
+	  .url = {
+		  .host = "localhost:8080",
+		  .path = "/",
+	  },
+	  .proto = "HTTP/1.1",
+	  .headers = {{"host", "localhost:8080"}},
+	  .content_length = 0,
+  };
+  http::Request current;
+  http::parse_request(current, message);
+  ASSERT_EQ(current, expected);
